bec217c7601aec41e2e61884577730675995e6a1
seo yong hyun <syh05247@naver.com>
2022-05-05 01:13:04 +0900

    @Controller @RestController 어노테이션 웹 기반 설계

---
+++ src/main/java/com/project/getinline/controller/api/APIPlaceController.java
@@ -0,0 +1,36 @@
+package com.project.getinline.controller.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+public class APIPlaceController {
+
+    @GetMapping("/places")
+    public List<String> getPlaces(){
+        return List.of("place1", "place2");
+    }
+
+    @PostMapping("/places")
+    public Boolean createPlace() {
+        return true;
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String getPlace(@PathVariable Integer placeId){
+        return "place" + placeId;
+    }
+
+    @PutMapping("/places/{placeId}")
+    public Boolean modifyPlace(@PathVariable Integer placeId){
+        return true;
+    }
+
+    @DeleteMapping("/places/{placeId}")
+    public Boolean removePage(@PathVariable Integer placeId){
+        return true;
+    }
+
+}
