1820ab123c0786bf366367fb018ae8fb7860108c
seo yong hyun <syh05247@naver.com>
2022-05-05 00:30:36 +0900

    Initial commit

---
+++ src/main/java/com/project/getinline/GetinlineApplication.java
@@ -0,0 +1,13 @@
+package com.project.getinline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetinlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetinlineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/getinline/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.project.getinline.controller;
+
+public class AuthController {
+}
